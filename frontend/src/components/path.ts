be7e72482f7de9decd340049dd48993dc1953e9d
Nramyanaga <ramyanaganagarur@gmail.com>
2025-10-28 16:44:09 -0400

    addjusted the path.js to not include api in the website

--- frontend/src/components/path.ts
+++ frontend/src/components/path.ts
@@ -14,7 +14,7 @@ const app_name = 'vasupradha.xyz';
 export function buildPath(route: string): string {
   if (import.meta.env.MODE !== 'development') {
     // Use HTTPS in production
-    return `https://${app_name}/api${route.startsWith('/') ? route : `/${route}`}`;
+    return `https://${app_name}${route.startsWith('/') ? route : `/${route}`}`;
   } else {
     // Local development
     return `http://localhost:5000${route.startsWith('/') ? route : `/${route}`}`;
