3f46a665237ef2cc3c45a70f79c8303ad26793b6
vasuramji52 <vasupradha.ramji@gmail.com>
2025-11-06 02:59:33 -0500

    Added the flutter app with login and register

---
+++ mobile/lib/services/api_service.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const baseUrl = 'http://172.26.64.1:5000/api';
+
+  static Future<http.Response> loginUser(String email, String password) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/login'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'email': email, 'password': password}),
+    );
+    return response;
+  }
+
+  static Future<http.Response> registerUser(
+    String firstName,
+    String lastName,
+    String email,
+    String password,
+  ) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/register'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'firstName': firstName,
+        'lastName': lastName,
+        'email': email,
+        'password': password,
+      }),
+    );
+    return response;
+  }
+}
---
+++ mobile/lib/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const darkTeal = Color(0xFF1B4B5A);
+  static const accentTeal = Color(0xFF2C6E7E);
+  static const gold = Color(0xFFFFD700);
+  static const bronze = Color(0xFFD4AF37);
+}
